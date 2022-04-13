25afdffe1a0a7c5763c0bf695fd00a8e3c9205b7
johnstef99 <john@johnstef.com>
2022-04-14 00:47:48 +0300

    fix indenting

--- examples/hello/src/hello.s
+++ examples/hello/src/hello.s
@@ -4,13 +4,12 @@
 _start:
         ldr     r0, =message
         bl      print
-		bl      exit
+        bl      exit
 
 exit:
-		mov 	r7, #0x1            // set sys call to 1 aka exit
-		mov 	r0, #0              // set exit code
-		swi 	0
-
+        mov     r7, #0x1            // set sys call to 1 aka exit
+        mov     r0, #0              // set exit code
+        swi     0
 
 .section .data
         message: .ascii "Hi I am john!\nHere is multi line a message\n\0"
