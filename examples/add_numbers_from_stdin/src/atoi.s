32410ad2611e218f9f1b60d4a0eed8f6ef4427b1
johnstef99 <john@johnstef.com>
2022-05-07 14:59:38 +0300

    atoi can handle negative numbers

--- examples/add_numbers_from_stdin/src/atoi.s
+++ examples/add_numbers_from_stdin/src/atoi.s
@@ -11,24 +11,42 @@ atoi:
         mov     r4, #0              // init r4 length counter
         mov     r6, #1              // init multiplier
         mov     r8, #10             // power
+        mov     r9, #0              // register to keep the sign
+
+        ldrb    r5, [r0]            // load the first char
+        cmp     r5, #0x2d           // check if char is '-' 
+        bne     length_loop         // if not '-' go to find length
+        sub     r9, r9, #1          // else set r9 to 0xffffffff
+        add     r0, r0, #1          // and increase the pointer r0
 
     length_loop:
         ldrb    r5, [r0, r4]        // load the char at pos r0[r4]
-        cmp     r5, #0x30           // check if is the null char
+        cmp     r5, #0x30           // check if char is below ascii 0
         blo     length_end          // exit loop
-        cmp     r5, #0x40           // check if is the null char
+        cmp     r5, #0x40           // check if char is over ascii 9
         bhs     length_end          // exit loop
         add     r4, r4, #1          // increate length counter
         b       length_loop         // check next char
     length_end:
         sub     r4, r4, #1          // sub 1 from length to get the last number
-        ldrb    r5, [r0, r4]        // load last number to r5
+        cmp     r4, #0              // check if length < 0
+        blt     no_more_numbers     // exit loop if it is
+        ldrb    r5, [r0, r4]        // load r5 = r0[r4]
         sub     r5, r5, #'0'        // convert ascii number to real number value
         mul     r7, r5, r6          // mul the number with the multiplier
         add     r1, r1, r7          // add multiplied num to result
         mul     r6, r8, r6          // increase the multiplier by the power of 10
-        cmp     r4, #0              // check if length is 0
-        bhi     length_end
+        b       length_end
 
+    no_more_numbers:
+        cmp     r1, #0              // check if resulted number is 0
+        beq     return              // exit if it is
+        cmp     r9, #0              // check if the sign register is 0
+        beq     return              // exit if it is
+        eor     r1, r1, r9          // xor r1 with 0xfffffffff
+        add     r1, r1, #1          // and add 1
+        sub     r0, r0, #1          // if number is negative restore r0 to original address
+
+    return:
         pop     {r4-r11}
         pop     {pc}
